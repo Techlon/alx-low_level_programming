457be2bb80531e9b8d112adb83397328c7e29dc4
Techlon <oluwagbemilesi@gmail.com>
2022-09-13 15:50:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * main - main Block
  * Description: Write a program that prints _putchar, followed by a new line.
  * The program should return 0
@@ -12,9 +12,9 @@ int main(void)
 	char putchar[] = "_putchar";
 	int i = 0;
 
-	while (putchar[i] != '\0')
+	while (putchar[i] != '0')
 	{
-		char(putchar[i]);
+		_putchar(putchar[i]);
 		i++;
 	}
 	_putchar('\n');
