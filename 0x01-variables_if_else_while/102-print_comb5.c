dbd286494c50adf41fa09310537109b1283a451a
Techlon <oluwagbemilesi@gmail.com>
2022-09-12 22:09:01 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,8 @@
  * Description: Print all possible combinations of two two-digit numbers.
  * Numbers should range from 0 to 99.
  * The two numbers should be separated by a space.
- * All numbers should be printed with two digits. 1 should be printed as 01.
+ * All numbers should be printed with two digits.
+ * 1 should be printed as 01.
  * Combination of numbers must be separated by a comma followed by a space.
  * Combinations of numbers should be printed in ascending order.
  *`00 01` and `01 00` are considered as the same combination.
