48f9a7f0b95e34cbe8968127fc0159cac7e68c68
Techlon <oluwagbemilesi@gmail.com>
2022-09-13 14:20:13 -0700

    New

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - main Block
+ * Description: Write a program that prints _putchar, followed by a new line.
+ * The program should return 0
+ * Return : 0
+ */
+
+char fay()
+{
+	char gre = '_putchar';
+		putchar(gre);
+	return (0);
+}
+
+
