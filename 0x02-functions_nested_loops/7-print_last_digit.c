0731a618851cbf0cfa765e9cf5aed6ed6e0698a7
Techlon <oluwagbemilesi@gmail.com>
2022-09-14 04:00:58 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry position
+ * Returns the value of the last digit
+ * Retun: int 0
+ */
+
+int print_last_digit(int ab)
+{
+	return ab % 10;
+}
+
