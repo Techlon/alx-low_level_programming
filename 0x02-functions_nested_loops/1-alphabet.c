6da4c3baed11e6bade72eb96d713454757eea2ca
Techlon <oluwagbemilesi@gmail.com>
2022-09-13 18:37:51 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ * Description: prints the alphabet in lower case
+ * followed by a new line
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 int _putchar(char stringOne);
+void print_alphabet(void);
