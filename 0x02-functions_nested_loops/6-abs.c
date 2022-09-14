462f12ea76384f0acc3562119f355326a76192ed
Techlon <oluwagbemilesi@gmail.com>
2022-09-14 03:21:16 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs(int) - entry point
+ * Descripton:function that computes the absolute value of an integer.
+ * @ab: the absolute value of ab.
+ * Return: int value.
+ * function _abs:the absolut value of ab
+ */
+
+int _abs(int ab)
+{
+	ab = ab - 0;
+
+	if (ab > 0)
+		_putchar ("ab");
+	else
+		ab * -;
+		_putchar ("ab");
+}
